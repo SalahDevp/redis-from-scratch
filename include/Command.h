6f208a370186427e736b69cc3cac3c3d5f9646ae
Salah Eddine Oukil <slhokli2002@gmail.com>
2024-08-08 13:10:47 +0100

    Add command factory

--- include/Command.h
+++ include/Command.h
@@ -4,8 +4,10 @@
 #include "Connection.h"
 #include "DataStore.h"
 #include "ResponseSerializer.h"
+#include "utils.h"
 #include <memory>
 #include <stdexcept>
+#include <string>
 
 class Command {
 public:
@@ -25,12 +27,31 @@ protected:
 
 class SetCommand : public Command {
 public:
+  SetCommand(DataStore &ds) : Command(ds) {};
   void execute(std::shared_ptr<Connection> &conn);
 };
 
 class GetCommand : public Command {
 public:
+  GetCommand(DataStore &ds) : Command(ds) {};
   void execute(std::shared_ptr<Connection> &conn);
+};
+
+class CommandFactory {
+public:
+  static std::unique_ptr<Command>
+  getCommand(const std::vector<std::string> &argv, DataStore &ds) {
+    std::string cmd = argv[0];
+    utils::strToLowerCase(cmd);
+
+    if (cmd == "get")
+      return std::make_unique<GetCommand>(ds);
+
+    if (cmd == "set")
+      return std::make_unique<SetCommand>(ds);
+
+    throw std::runtime_error("command is invalid.");
+  }
 };
 
 #endif
