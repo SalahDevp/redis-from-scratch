7050d860c6e161e373fb0e31afe55de9926ab253
Salah Eddine Oukil <slhokli2002@gmail.com>
2024-08-18 16:21:40 +0100

    Add cli client

--- cli-client.py
+++ cli-client.py
@@ -5,6 +5,17 @@ server_ip = "127.0.0.1"
 server_port = 6973
 
 
+def handleResponse(args):
+    i = 0
+    while i < len(args):
+        if args[i] == "+":
+            print(args[i][1:])
+        elif args[i].startswith("$"):
+            print(args[i + 1])
+
+        i += 1
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.connect((server_ip, server_port))
 
@@ -18,6 +29,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         for arg in args:
             resp_m += f"${len(arg)}\r\n{arg}\r\n"
 
-        sock.sendall(message.encode())
+        sock.sendall(resp_m.encode())
         rec = sock.recv(1024)
-        print(rec.decode())
+        handleResponse(rec.decode().split("\r\n"))
